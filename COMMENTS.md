26fe832490357bf507d9996a25c6477bec3a610d
Samuel Ferino <sferino@lenovo.com>
2024-03-23 02:24:05 -0300

    organizando o repositório, primeira reflexão

---
+++ COMMENTS.md
@@ -0,0 +1,36 @@
+# Registro de reflexões
+
+Dia 23/Março (madrugada)
+
+- Avaliando os requisitos e ferramentas open source necesárias. 
+
+    -  Para Cloud, eu decidi usar o GPC já que a vaga faz menção a ela (espero que não esteja cara kkk).
+
+    - Para o requisito de IaaC - criação da VMS, estou pensando em usar ou Terraform, ferramentas famosas por configurar infra de forma declarativa. 
+
+    -  Para pipeline de CD, tenho duas opções gratuitas famosas: github actions e gitlab ci. Pessoalmente não vejo muita diferença entre uma e outra
+
+        - Verifiquei que não tem testes automatizados. Acho que no final vale a pena adicionar. 
+
+    - Acredito que o uso de API Gateway é bastante fundamental. O Kong é bastante famoso como uma solução open-source.
+
+    - Para aumentar a resiliência, pretendo instalar um cluster kubernetes, Pretendo usar o Ansible para instalação de um cluster kubernetes microk8s. A instalação dele é mais simples que a do kubeadmin.
+
+    - configura o pacote de observabilidade do microk8s. Dessa forma, já habilito o monitoramento do cluster
+
+
+- Scripts a serem criados
+
+    - Dockerfile
+
+    -  Makefile
+
+    - pasta .github (Github Actions)
+
+    - Deployment/Service (Kubernetes)
+
+    - pasta terraform
+
+    - pasta ansible
+
+(Vou dormir)
---
+++ app/api.py
@@ -0,0 +1,51 @@
+from flask import Flask
+from flask import jsonify
+from flask import request
+
+
+app_name = 'comentarios'
+app = Flask(app_name)
+app.debug = True
+
+comments = {}
+
+
+@app.route('/api/comment/new', methods=['POST'])
+def api_comment_new():
+    request_data = request.get_json()
+
+    email = request_data['email']
+    comment = request_data['comment']
+    content_id = '{}'.format(request_data['content_id'])
+
+    new_comment = {
+            'email': email,
+            'comment': comment,
+            }
+
+    if content_id in comments:
+        comments[content_id].append(new_comment)
+    else:
+        comments[content_id] = [new_comment]
+
+    message = 'comment created and associated with content_id {}'.format(content_id)
+    response = {
+            'status': 'SUCCESS',
+            'message': message,
+            }
+    return jsonify(response)
+
+
+@app.route('/api/comment/list/<content_id>')
+def api_comment_list(content_id):
+    content_id = '{}'.format(content_id)
+
+    if content_id in comments:
+        return jsonify(comments[content_id])
+    else:
+        message = 'content_id {} not found'.format(content_id)
+        response = {
+                'status': 'NOT-FOUND',
+                'message': message,
+                }
+        return jsonify(response), 404
