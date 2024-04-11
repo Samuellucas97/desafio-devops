aa65a3844eb8e9684803320585385d8ff42ee551
Samuel Ferino <sferino@lenovo.com>
2024-04-11 10:57:42 -0300

    mudando de requirements txt para poetry

---
+++ app/config.py
@@ -0,0 +1,3 @@
+LOGSTASH_HOST='localhost'
+LOGSTASH_PORT=5000 
+LOGGING_FORMAT='%(asctime)s - %(name)s - %(levelname)s - %(message)s'
--- app/main.py
+++ app/main.py
@@ -2,6 +2,9 @@ from flask import Flask
 from flask import jsonify
 from flask import request
 
+from http import HTTPStatus
+
+from api.utils import logger
 
 app_name = 'comentarios'
 app = Flask(app_name)
@@ -30,27 +33,30 @@ def api_comment_new():
 
     message = 'comment created and associated with content_id {}'.format(content_id)
     response = {
-            'status': 'SUCCESS',
+            'status': HTTPStatus.OK,
             'message': message,
             }
-    return jsonify(response)
+    return jsonify(response), HTTPStatus.OK
 
 
 @app.route('/api/comment/list/<content_id>')
 def api_comment_list(content_id):
     content_id = '{}'.format(content_id)
+    app.logger.info('Trying to retrieve comment')
+    
 
     if content_id in comments:
         return jsonify(comments[content_id])
     else:
+        app.logger.error('Comment id %s does not exist', content_id)
         message = 'content_id {} not found'.format(content_id)
         response = {
-                'status': 'NOT-FOUND',
+                'status': HTTPStatus.NOT_FOUND,
                 'message': message,
                 }
-        return jsonify(response), 404
+        return jsonify(response), HTTPStatus.NOT_FOUND
 
 
 @app.route('/health')
 def api_healthcheck():
-    return {}, 200
+    return {}, HTTPStatus.OK
